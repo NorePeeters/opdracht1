0c8c741e5b8b2aca1fbbd023bc3ea783744356ca
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-01-25 14:55:24 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,18 +1,3 @@
 basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        # . # . #
-        # . . . #
-        . # . # .
-        . . # . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(100)
+    basic.showString("Nore")
 })
